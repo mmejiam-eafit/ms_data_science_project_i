26da058181cbea62de3adfe09e873d89f74acce5
dcardonap <dscardonap@eafit.edu.co>
2019-04-15 00:41:43 -0500

    Check parametric distribution of distances

--- eigenfaces/utils/ImageUtils.py
+++ eigenfaces/utils/ImageUtils.py
@@ -186,4 +186,84 @@ def visualizeOutlierInfo2(distance_dict,dataset,labels):
         ax.invert_yaxis()
         ax.invert_xaxis()
 
- # =======
+ # ============
+
+#==== Check parametric distribution of distances
+import scipy
+from sklearn.preprocessing import StandardScaler
+import scipy.stats
+
+def check_parametricDistribu_distances(y):
+    x = np.arange(len(y))
+    size = len(y)
+    y_df = pd.DataFrame(y)
+    sc=StandardScaler() 
+    sc.fit(y_df)
+    y_std =sc.transform(y_df)
+    y_std = y_std.flatten()
+    
+    # Set up list of candidate distributions to use
+    # See https://docs.scipy.org/doc/scipy/reference/stats.html for more
+    dist_names = ['beta',
+              'expon',
+              'gamma',
+              'lognorm',
+              'norm',
+              'pearson3',
+              'weibull_min', 
+              'weibull_max',
+             'chi2']
+    
+    # Set up empty lists to stroe results
+    chi_square = []
+    p_values = []
+    
+    # Set up 50 bins for chi-square test
+    # Observed data will be approximately evenly distrubuted aross all bins
+    percentile_bins = np.linspace(0,100,51)
+    percentile_cutoffs = np.percentile(y_std, percentile_bins)
+    observed_frequency, bins = (np.histogram(y_std, bins=percentile_cutoffs))
+    cum_observed_frequency = np.cumsum(observed_frequency)
+    
+    # Loop through candidate distributions
+    
+    for distribution in dist_names:
+        # Set up distribution and get fitted distribution parameters
+        dist = getattr(scipy.stats, distribution)
+        param = dist.fit(y_std)
+        
+        # Obtain the KS test P statistic, round it to 5 decimal places
+        p = scipy.stats.kstest(y_std, distribution, args=param)[1]
+        p = np.around(p, 5)
+        p_values.append(p)
+        
+        # Get expected counts in percentile bins
+        # This is based on a 'cumulative distrubution function' (cdf)
+        
+        cdf_fitted = dist.cdf(percentile_cutoffs, *param[:-2], loc=param[-2],scale=param[-1])
+        expected_frequency = []
+        
+        for bin in range(len(percentile_bins)-1):
+            expected_cdf_area = cdf_fitted[bin+1] - cdf_fitted[bin]
+            expected_frequency.append(expected_cdf_area)
+            
+       # calculate chi-squared
+        expected_frequency = np.array(expected_frequency) * size
+        cum_expected_frequency = np.cumsum(expected_frequency)
+        ss = sum (((cum_expected_frequency - cum_observed_frequency) ** 2) / cum_observed_frequency)
+        chi_square.append(ss)
+        
+    # Collate results and sort by goodness of fit (best at top)
+
+    results = pd.DataFrame()
+    results['Distribution'] = dist_names
+    results['chi_square'] = chi_square
+    results['p_value'] = p_values
+    results.sort_values(['chi_square'], inplace=True)
+    
+    # Report results
+
+    print ('\nDistributions sorted by goodness of fit:')
+    print ('----------------------------------------')
+    print (results)
+
